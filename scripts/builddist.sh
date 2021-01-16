f67de2bcc7debf8b1acc94a8e6725df71b673723
David Freese <w1hkj@bellsouth.net>
2021-01-16 13:46:14 -0600

    mingw build scripts
      * remove regex library dependency

--- scripts/builddist.sh
+++ scripts/builddist.sh
@@ -1,18 +1,13 @@
 # build file to generate the distribution binary tarball
 
 make clean
-./configure --prefix=/tmp/comptext-build
-make install-strip
-tar czf comptext-$1.bin.tgz -C /tmp/comptext-build/bin comptext
-
-make clean
 
 ./configure \
   $PKGCFG \
   $CROSSCFG \
   --with-ptw32=$PREFIX/i686-w64-mingw32.static \
   --enable-static \
-  PTW32_LIBS="-lpthread -lpcreposix -lpcre -lregex" \
+  PTW32_LIBS="-lpthread -lpcreposix -lpcre" \
   FLTK_CONFIG=$PREFIX/bin/i686-w64-mingw32.static-fltk-config \
 
 make
